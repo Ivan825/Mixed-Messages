6d7683105d9dc811b674ecd4b0f97c4f7e25bda9
IvanB <ivanbhargava@gmail.com>
2021-10-25 16:18:38 +0530

    Project Completed

---
+++ script.js
@@ -0,0 +1,4 @@
+let p1= ['Hello','Hi','Sup','GET OUT','BYE BYE'];
+let p2= ['loser','Champion','shrek','Vampire','Gargoyle'];
+let p3= ['wathcha doin',' I hate you','I love you','leave me alone','let me be'];
+ console.log(`${p1[Math.floor(Math.random()*5)]} ${p2[Math.floor(Math.random()*5)]} ${p3[Math.floor(Math.random()*5)]}`);
